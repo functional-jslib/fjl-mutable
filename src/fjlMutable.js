2517f7582c74fc2a1c8fbe1cc28484b234e22368
Ely De La Cruz <elycruz@elycruz.com>
2017-11-10 19:52:43 -0500

    initial commit and initial idea.

---
+++ src/fjlMutable.js
@@ -0,0 +1,87 @@
+import {curry, flip, flip3} from 'fjl';
+
+import {errorIfNotType} from 'fjl-error-throwing';
+
+export const
+
+    _descriptorForSettable = Type => {
+        let _value;
+        return {
+            get: function () {
+                return _value;
+            },
+            set: function (value) {
+                _value = errorIfNotTypeOnTarget(Type, target, propName, value);
+            }
+        };
+    },
+
+    _makeEnumerableDescriptor = descriptor => {
+        descriptor.enumerable = true;
+        return descriptor;
+    },
+
+    errorIfNotTypeOnTarget$ = (Type, target, propName, propValue) => {
+        errorIfNotType((target).constructor.name, propName, propValue, Type);
+        return propValue;
+    },
+
+    defineProp$ = (Type, propName, target) => {
+        const descriptor = _descriptorForSettable(Type);
+        Object.defineProperty(target, propName, descriptor);
+        return [target, descriptor];
+    },
+
+    defineEnumProp$ = (Type, propName, target) =>
+        _makeEnumerableDescriptor (
+            defineProp$(Type, propName, target)
+        ),
+
+    errorIfNotTypeOnTarget = curry(errorIfNotTypeOnTarget$),
+
+    defineProp = curry(defineProp$),
+
+    defineEnumProp = curry(defineEnumProp$),
+
+    definePropArray = defineProp(Array),
+
+    definePropBoolean = defineProp(Boolean),
+
+    definePropFunction = defineProp(Function),
+
+    definePropNumber = defineProp(Number),
+
+    definePropString = defineProp(String),
+
+    defineEnumPropArray = defineEnumProp(Array),
+
+    defineEnumPropBoolean = defineEnumProp(Boolean),
+
+    defineEnumPropFunction = defineEnumProp(Function),
+
+    defineEnumPropNumber = defineEnumProp(Number),
+
+    defineEnumPropString = defineEnumProp(String)
+
+;
+
+export default {
+    defineProp,
+    defineProp$,
+    definePropArray,
+    definePropBoolean,
+    definePropFunction,
+    definePropNumber,
+    definePropString,
+    defineEnumProp,
+    defineEnumProp$,
+    defineEnumPropArray,
+    defineEnumPropBoolean,
+    defineEnumPropFunction,
+    defineEnumPropNumber,
+    defineEnumPropString,
+    errorIfNotTypeOnTarget,
+    errorIfNotTypeOnTarget$,
+    _descriptorForSettable,
+    _makeEnumerableDescriptor
+}
